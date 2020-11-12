c476771322af264f27e9a761f974d4e8effa2d4f
Dan Sullivan <dansullivan@gmail.com>
2020-11-11 21:34:27 -0500

    fixed position header and footers

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,140 @@ export default function Home() {
   return (
     <div className={holyGrail.mainContainer}>
       <Head>
-        <title>Holy Grail App</title>
+        <title>Holy Grail</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <header className={holyGrail.header}>
-        Header
+        <div className={holyGrail.flexContainerColumnPageWidth}>
+          <div className={holyGrail.leftColumn}>
+          <div className={holyGrail.flexContainerLeftAlign}>
+            <div className={holyGrail.menuItem}>
+              Left 1
+            </div>
+            <div className={holyGrail.menuItem}>
+              Left 2
+            </div>
+          </div>
+          </div>
+          <div className={holyGrail.centerColumn}>
+            <div className={holyGrail.flexContainerCenterAlign}>
+              <div className={holyGrail.menuItem}>
+                Middle
+              </div>
+            </div>
+          </div>
+          <div className={holyGrail.rightColumn}>
+            <div className={holyGrail.flexContainerRightAlign}>
+              <div className={holyGrail.menuItem}>
+                One
+              </div>
+              <div className={holyGrail.menuItem}>
+                Two
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
 
       <main className={holyGrail.main}>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
       </main>
 
       <footer className={holyGrail.footer}>
-        Footer
+        <div className={holyGrail.flexContainerColumnPageWidth}>
+            <div className={holyGrail.leftColumn}>
+            <div className={holyGrail.flexContainerLeftAlign}>
+              <div className={holyGrail.menuItem}>
+                Left 1
+              </div>
+              <div className={holyGrail.menuItem}>
+                Left 2
+              </div>
+            </div>
+            </div>
+            <div className={holyGrail.centerColumn}>
+              <div className={holyGrail.flexContainerCenterAlign}>
+                <div className={holyGrail.menuItem}>
+                  Middle
+                </div>
+              </div>
+            </div>
+            <div className={holyGrail.rightColumn}>
+              <div className={holyGrail.flexContainerRightAlign}>
+                <div className={holyGrail.menuItem}>
+                  One
+                </div>
+                <div className={holyGrail.menuItem}>
+                  Two
+                </div>
+              </div>
+            </div>
+          </div>
       </footer>
     </div>
   )
