c1c665acad229c6488a5a06bbf3537179351fc7f
Dan Sullivan <dansullivan@gmail.com>
2021-03-28 07:44:00 -0400

    abstract divider and highlights

--- constants.js
+++ constants.js
@@ -1,18 +1,21 @@
 export const STARTX                         = 15;
 export const STARTY                         = 15;
-export const DOCUMENT_WIDTH                 = 612
-export const DOCUMENT_HEIGHT                = 792
-export const UNITS                          = 'pt'
+export const DOCUMENT_WIDTH                 = 612;
+export const DOCUMENT_HEIGHT                = 792;
+export const UNITS                          = 'pt';
+export const DIVIDER_COLOR                  = 'red';
+export const BACKGROUND_COLOR               = 'black';
+export const TEXT_COLOR                     = 'white';
 
 export const LEFT_PANEL_XPOS                = 0;
 export const LEFT_PANEL_YPOS                = 0;
 export const LEFT_PANEL_PERCENTAGE          = .3;
 export const LEFT_PANEL_WIDTH               = LEFT_PANEL_PERCENTAGE * DOCUMENT_WIDTH;
-export const LEFT_PANEL_COLOR               = 'black';
+export const LEFT_PANEL_COLOR               = BACKGROUND_COLOR;
 
 export const RIGHT_PANEL_XPOS               = LEFT_PANEL_WIDTH;
 export const RIGHT_PANEL_YPOS               = 0;
-export const RIGHT_PANEL_COLOR              = 'gray';
+export const RIGHT_PANEL_COLOR              = BACKGROUND_COLOR;
 export const RIGHT_PANEL_PERCENTAGE         = 1 - LEFT_PANEL_PERCENTAGE;
 export const RIGHT_PANEL_WIDTH              = RIGHT_PANEL_PERCENTAGE * DOCUMENT_WIDTH;
 
@@ -34,7 +37,7 @@ export const VERTICAL_DIVIDER_STROKE_WIDTH  = 2;
 export const VERTICAL_DIVIDER_XPOS          = LEFT_PANEL_WIDTH;
 export const VERTICAL_DIVIDER_YPOS          = 0;
 export const VERTICAL_DIVIDER_HEIGHT        = DOCUMENT_HEIGHT;
-export const VERTICAL_DIVIDER_COLOR         = 'red';
+export const VERTICAL_DIVIDER_COLOR         = DIVIDER_COLOR;
 
 export const ADDRESS_LINE_STROKE_WIDTH      = 2;
 export const ADDRESS_LINE_SPACING           = 12;
@@ -42,7 +45,7 @@ export const ADDRESS_LINE_YPOS              = ADDRESS_YPOS + ADDRESS_SIZE + ADDR
 export const ADDRESS_LINE_WIDTH             = LEFT_PANEL_WIDTH * (1/3);
 export const ADDRESS_LINE_X2                = VERTICAL_DIVIDER_XPOS;
 export const ADDRESS_LINE_X1                = VERTICAL_DIVIDER_XPOS - ADDRESS_LINE_WIDTH;
-export const ADDRESS_LINE_COLOR             = 'red';
+export const ADDRESS_LINE_COLOR             = DIVIDER_COLOR;
 
 export const PHONE_NUMBER_SIZE              = ADDRESS_SIZE;
 export const PHONE_NUMBER_WEIGHT            = 400;
