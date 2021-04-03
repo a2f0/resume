a6f52c35420d8bd1a585b92f34c23ea112648391
Dan Sullivan <dansullivan@gmail.com>
2021-04-03 07:33:27 -0400

    change pdf to typescript (#7)

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout"
-import PDFObject from "pdfobject";
+import PDFObject, { jsPDFOptions, TextOptionsLight } from "pdfobject";
 import { jsPDF } from "jspdf";
 import * as Constants from '../constants'
 import resume from '../resume.json';
-import svgR from '../util.js'
 require('../public/fonts/Roboto-Regular-normal');
 
 export default function Pdf() {
@@ -16,7 +15,7 @@ export default function Pdf() {
 
   const createPDF = () => {
 
-    const options = {
+    const options: jsPDFOptions = {
       orientation: 'portrait',
       unit: Constants.UNITS,
       format: 'letter'
@@ -47,7 +46,7 @@ export default function Pdf() {
     doc.setFont("Helvetica")
     doc.setFontSize(Constants.FIRST_NAME_SIZE)
     doc.setTextColor(Constants.FIRST_NAME_COLOR)
-    const first_name_options = { baseline: "middle" }
+    const first_name_options: TextOptionsLight = { baseline: "middle" }
     doc.text(
       resume.first_name,
       Constants.FIRST_NAME_XPOS,
@@ -58,7 +57,7 @@ export default function Pdf() {
     // Last Name
     var firstNameWidth = doc.getTextWidth(resume.first_name);
     doc.setTextColor(Constants.LAST_NAME_COLOR)
-    const last_name_options = { baseline: "middle" }
+    const last_name_options: TextOptionsLight = { baseline: "middle" }
     doc.text(
       resume.last_name,
       Constants.FIRST_NAME_XPOS + firstNameWidth,
@@ -70,7 +69,7 @@ export default function Pdf() {
     doc.setFont("Helvetica")
     doc.setFontSize(Constants.ADDRESS_SIZE)
     doc.setTextColor(Constants.ADDRESS_COLOR)
-    const city_state_options = { baseline: "middle" }
+    const city_state_options: TextOptionsLight = { baseline: "middle" }
     doc.text(resume.city_state, Constants.ADDRESS_XPOS, Constants.ADDRESS_YPOS_MIDDLE, city_state_options );
 
     // Vertical Divider Line (Rectangle to establish width)
@@ -97,21 +96,21 @@ export default function Pdf() {
     doc.setFont("Helvetica")
     doc.setFontSize(Constants.PHONE_NUMBER_SIZE)
     doc.setTextColor(Constants.PHONE_NUMBER_WEIGHT)
-    const phone_number_options = { baseline: "middle" }
+    const phone_number_options: TextOptionsLight = { baseline: "middle" }
     doc.text(resume.phone_number, Constants.PHONE_NUMBER_XPOS, Constants.PHONE_NUMBER_YPOS, first_name_options );
 
     // Email Address
     doc.setFont("Helvetica")
     doc.setFontSize(Constants.EMAIL_SIZE)
     doc.setTextColor(Constants.EMAIL_COLOR)
-    const email_options = { baseline: "middle" }
+    const email_options: TextOptionsLight = { baseline: "middle" }
     doc.text(resume.email, Constants.EMAIL_XPOS, Constants.EMAIL_YPOS, email_options );
 
     // Experience Header
     doc.setFont("Helvetica")
     doc.setFontSize(Constants.EXPERIENCE_HEADER_SIZE)
     doc.setTextColor(Constants.HEADER_COLOR)
-    const experience_header_options = { baseline: "middle" }
+    const experience_header_options: TextOptionsLight = { baseline: "middle" }
     doc.text(Constants.EXPERIENCE_HEADER, Constants.EXPERIENCE_HEADER_XPOS, Constants.EXPERIENCE_HEADER_YPOS, experience_header_options );
 
     return doc.output('datauristring');
