bc5b60b4459ec6af348a76cf43f33c3d0f97f14e
Dan Sullivan <dansullivan@gmail.com>
2021-04-05 05:48:59 -0400

    _app.js -> _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import "../styles/globals.css";
+import { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
