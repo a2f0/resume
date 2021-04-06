83b8c775210e3785805ca46d231e35f594ba6313
Dan Sullivan <dansullivan@gmail.com>
2021-04-06 06:33:44 -0400

    color inverter

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,57 @@
 import Head from 'next/head';
 import holyGrail from '../styles/HolyGrail.module.css';
 import Link from 'next/link';
+import ColorInverter from './ColorInverter';
+import Color from 'color';
 
-const Header = () => (
-  <>
-    <Head>
-      <title>Resume</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <header className={holyGrail.header}>
-      <div className={holyGrail.flexContainerColumnPageWidth}>
-        <div className={holyGrail.leftColumn}>
-          <div className={holyGrail.flexContainerLeftAlign}>
-            <div className={holyGrail.menuItem}>
-              <Link href="/">SVG</Link>
+const Header = () => {
+  const black = Color('#FFF');
+  const white = Color('#000');
+
+  return (
+    <>
+      <Head>
+        <title>Resume</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={holyGrail.header}>
+        <div className={holyGrail.flexContainerColumnPageWidth}>
+          <div className={holyGrail.leftColumn}>
+            <div className={holyGrail.flexContainerLeftAlign}>
+              <div className={holyGrail.menuItem}>
+                <Link href="/">SVG</Link>
+              </div>
+              <div className={holyGrail.menuItem}>
+                <Link href="/pdf">PDF</Link>
+              </div>
             </div>
-            <div className={holyGrail.menuItem}>
-              <Link href="/pdf">PDF</Link>
+          </div>
+          <div className={holyGrail.centerColumn}>
+            <div className={holyGrail.flexContainerCenterAlign}>
+              <div className={holyGrail.menuItem}>
+                <ColorInverter
+                  degrees={135}
+                  foreground={black}
+                  background={white}
+                />
+                <ColorInverter
+                  degrees={45}
+                  foreground={white}
+                  background={black}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={holyGrail.rightColumn}>
+            <div className={holyGrail.flexContainerRightAlign}>
+              <div className={holyGrail.menuItem}>One</div>
+              <div className={holyGrail.menuItem}>Two</div>
             </div>
           </div>
         </div>
-        <div className={holyGrail.centerColumn}>
-          <div className={holyGrail.flexContainerCenterAlign}>
-            <div className={holyGrail.menuItem}>Middle</div>
-          </div>
-        </div>
-        <div className={holyGrail.rightColumn}>
-          <div className={holyGrail.flexContainerRightAlign}>
-            <div className={holyGrail.menuItem}>One</div>
-            <div className={holyGrail.menuItem}>Two</div>
-          </div>
-        </div>
-      </div>
-    </header>
-  </>
-);
+      </header>
+    </>
+  );
+};
 
 export default Header;
