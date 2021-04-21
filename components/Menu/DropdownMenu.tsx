a66c3fa8c775c8c12e37bc0cc95900ec62d724f3
Dan Sullivan <dansullivan@gmail.com>
2021-04-21 08:02:56 -0400

    breaking menu into separate components

--- components/Menu/DropdownMenu.tsx
+++ components/Menu/DropdownMenu.tsx
@@ -1,55 +1,12 @@
 import React, {useRef} from 'react';
 import {useDetectOutsideClick} from '../../library/useDetectOutsideClick';
-import styled, {css} from 'styled-components';
+import {MenuContainer} from './MenuContainer';
 import {MenuButton} from './MenuButton';
-
-const MenuContainer = styled.div`
-position: relative,
-display: flex,
-justify-content: center,
-align-items: center,
-`;
-
-interface IProps {
-  isActive: boolean;
-}
+import {Menu} from './Menu';
 
 interface MenuProps {
   label: string;
 }
-
-const Menu = styled.div<IProps>`
-  background: #ffffff;
-  border-radius: 8px;
-  position: absolute;
-  // left: 0;
-  width: 300px;
-  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.3);
-  opacity: 0;
-  visibility: hidden;
-  transform: translateY(-20px);
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-  li {
-    border-bottom: 1px solid #dddddd;
-  }
-  li a {
-    text-decoration: none;
-    color: #333333;
-    padding: 15px 20px;
-    display: block;
-  }
-  ${({isActive}) =>
-    isActive &&
-    css`
-      opacity: 1;
-      visibility: visible;
-      transform: translateY(0);
-    `}
-`;
 
 // https://codesandbox.io/s/dropdown-menu-jzldk?file=/src/App.js:397-441
 export default function DropdownMenu(props: MenuProps) {
---
+++ components/Menu/MenuContainer.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const MenuContainer = styled.div`
+  position: relative,
+  display: flex,
+  justify-content: center,
+  align-items: center,
+`;
