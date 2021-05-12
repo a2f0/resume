1d6536c5c138ed75b71b7556ba00acd55a628bde
Dan Sullivan <dansullivan@gmail.com>
2021-05-12 11:57:15 -0400

    update to reccomended a-record method

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,15 +46,29 @@ data "aws_route53_zone" "resume" {
   name = var.domain
 }
 
-resource "aws_route53_record" "resume" {
+resource "aws_route53_record" "resume-ns" {
   allow_overwrite = true
   name            = var.domain
-  ttl             = 600
+  ttl             = 60
   type            = "NS"
   zone_id         = data.aws_route53_zone.resume.zone_id
 
   records = [
-    vercel_domain.resume.intended_nameservers[0],
-    vercel_domain.resume.intended_nameservers[1],
+    "ns-468.awsdns-58.com",
+    "ns-844.awsdns-41.net",
+    "ns-1621.awsdns-10.co.uk",
+    "ns-1372.awsdns-43.org"
+  ]
+}
+
+resource "aws_route53_record" "resume-a" {
+  allow_overwrite = true
+  name            = var.domain
+  ttl             = 60
+  type            = "A"
+  zone_id         = data.aws_route53_zone.resume.zone_id
+
+  records = [
+    "76.76.21.21"
   ]
 }
