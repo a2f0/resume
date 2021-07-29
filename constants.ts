a01e09bdb006f4e81259f4d2ba59b6a2efda5b20
Dan Sullivan <dansullivan@gmail.com>
2021-07-28 21:26:28 -0400

    further spacing adjustments

--- constants.ts
+++ constants.ts
@@ -2,27 +2,28 @@
 import path from 'path';
 
 export const TEST_DOWNLOAD_DIR                      = path.join(__dirname, 'test', 'tempDownload');
-export const STARTX                                 = 18;
-export const STARTY                                 = 18;
+export const STARTX                                 = 18; // .25 inches from edge of page
+export const STARTY                                 = 18; // .25 inches from edge of page
 export const RIGHT_PANEL_STARTY                     = 77;
 export const DOCUMENT_WIDTH                         = 612;
 export const DOCUMENT_HEIGHT                        = 792;
 export const PIXELS_PER_POINT                       = .75;
+export const STANDARD_FONT_SIZE                     = 9;
 export const UNITS                                  = 'pt';
 export const BACKGROUND_COLOR                       = 'black';
 export const TEXT_COLOR                             = 'white';
-export const HEADER_SIZE                            = 14;
-export const HEADER_SPACING                         = 6;
-export const EXPERIENCE_HEADER                      = 'EXPERIENCE';
+export const HEADER_SIZE                            = 12;
+export const HEADER_SPACING                         = 4;
+export const EXPERIENCE_HEADER                      = 'Experience';
 export const FONT_FAMILY                            = 'Helvetica';
 export const HYPEN_SPACING                          = 4;
 export const DARK                                   = '#000000';
 export const LIGHT                                  = '#ffffff';
-export const CENTER_BULLET_MARGIN                   = 10;
+export const CENTER_BULLET_MARGIN                   = 8;
 
 export const LEFT_PANEL_XPOS                        = 0;
 export const LEFT_PANEL_YPOS                        = 0;
-export const LEFT_PANEL_PERCENTAGE                  = .27;
+export const LEFT_PANEL_PERCENTAGE                  = .25;
 export const LEFT_PANEL_WIDTH                       = LEFT_PANEL_PERCENTAGE * DOCUMENT_WIDTH;
 export const LEFT_PANEL_COLOR                       = BACKGROUND_COLOR;
 export const LEFT_PANEL_MARGIN                      = 18
@@ -33,7 +34,7 @@ export const RIGHT_PANEL_COLOR                      = BACKGROUND_COLOR;
 export const RIGHT_PANEL_PERCENTAGE                 = 1 - LEFT_PANEL_PERCENTAGE;
 export const RIGHT_PANEL_WIDTH                      = RIGHT_PANEL_PERCENTAGE * DOCUMENT_WIDTH;
 
-export const FIRST_NAME_SIZE                        = 17;
+export const FIRST_NAME_SIZE                        = 15;
 export const FIRST_NAME_WEIGHT                      = 400;
 export const FIRST_NAME_COLOR                       = 'white';
 export const FIRST_NAME_XPOS                        = STARTX;
@@ -45,7 +46,7 @@ export const LAST_NAME_WEIGHT                       = FIRST_NAME_WEIGHT;
 export const LAST_NAME_YPOS                         = STARTY;
 export const LAST_NAME_YPOS_MIDDLE                  = LAST_NAME_YPOS + (LAST_NAME_SIZE/2);
 
-export const ADDRESS_SIZE                           = 10;
+export const ADDRESS_SIZE                           = STANDARD_FONT_SIZE;
 export const ADDRESS_WEIGHT                         = 400;
 export const ADDRESS_COLOR                          = 'white';
 export const ADDRESS_XPOS                           = STARTX;
@@ -87,13 +88,13 @@ export const EXPERIENCE_HEADER_YPOS                 = RIGHT_PANEL_STARTY;
 export const POSITION_VERTICAL_SPACING              = 12;
 
 export const POSITION_TITLE_WEIGHT                  = 400;
-export const POSITION_TITLE_SIZE                    = 10;
+export const POSITION_TITLE_SIZE                    = STANDARD_FONT_SIZE;
 export const POSITION_TITLE_COLOR                   = 'white';
 export const POSITION_TITLE_XPOS                    = RIGHT_PANEL_STARTX;
 export const POSITION_TITLE_YPOS_START              = EXPERIENCE_HEADER_YPOS + POSITION_TITLE_SIZE + HEADER_SPACING
 
 export const POSITION_DATE_RANGE_WEIGHT             = 400;
-export const POSITION_DATE_RANGE_SIZE               = 10;
+export const POSITION_DATE_RANGE_SIZE               = STANDARD_FONT_SIZE;
 export const POSITION_DATE_RANGE_COLOR              = 'white';
 
 export const POSITION_COMPANY_WEIGHT                = 400;
@@ -107,30 +108,30 @@ export const POSITION_ACCOMPLISHMENT_SIZE           = POSITION_TITLE_SIZE;
 export const POSITION_ACCOMPLISHMENT_COLOR          = 'white';
 export const POSITION_ACCOMPLISHMENT_BULLET_RADIUS  = 1
 export const POSITION_ACCOMPLISHMENT_BULLET_XPOS    = RIGHT_PANEL_STARTX + POSITION_ACCOMPLISHMENT_BULLET_RADIUS * 2
-export const POSITION_ACCOMPLISHMENT_BULLET_MARGIN  = 6
+export const POSITION_ACCOMPLISHMENT_BULLET_MARGIN  = 5
 export const POSITION_ACCOMPLISHMENT_XPOS           = POSITION_ACCOMPLISHMENT_BULLET_XPOS + POSITION_ACCOMPLISHMENT_BULLET_MARGIN;
 export const POSITION_ACCOMPLISHMENT_MAX_WIDTH      = RIGHT_PANEL_WIDTH - ( RIGHT_PANEL_MARGIN * 2) - POSITION_ACCOMPLISHMENT_BULLET_MARGIN + POSITION_ACCOMPLISHMENT_BULLET_RADIUS;
 
-export const POSITION_BULLET_RADIUS                 = 3;
+export const POSITION_BULLET_RADIUS                 = 2.75;
 
 export const EDUCATION_HEADER_WEIGHT                = 400;
 export const EDUCATION_HEADER_SIZE                  = HEADER_SIZE;
 export const EDUCATION_HEADER_XPOS                  = RIGHT_PANEL_STARTX;
-export const EDUCATION_HEADER                       = 'EDUCATION';
+export const EDUCATION_HEADER                       = 'Education';
 export const EDUCATION_WEIGHT                       = 400;
 export const EDUCATION_SIZE                         = ADDRESS_SIZE;
 export const EDUCATION_COLOR                        = 'white';
 export const EDUCATION_XPOS                         = RIGHT_PANEL_STARTX;
 
-export const EDUCATION_BULLET_RADIUS                = 3;
+export const EDUCATION_BULLET_RADIUS                = POSITION_BULLET_RADIUS;
 export const EDUCATION_VERTICAL_SPACING             = POSITION_VERTICAL_SPACING;
 
 export const INTERNET_PRESENCES_HEADER_YPOS         = DOCUMENT_HEIGHT - 97;
-export const INTERNET_PRESENCES_HEADER_SIZE         = 14;
+export const INTERNET_PRESENCES_HEADER_SIZE         = HEADER_SIZE;
 
 export const INTERNET_PRESENCES_LINE_SPACING        = 4;
 export const INTERNET_PRESENCES_LINE_YPOS           = INTERNET_PRESENCES_HEADER_YPOS + (INTERNET_PRESENCES_HEADER_SIZE/2) + INTERNET_PRESENCES_LINE_SPACING;
 export const INTERNET_PRESENCES_LINE_WIDTH          = LEFT_PANEL_WIDTH * (1/10);
 
 export const INTERNET_PRESENCES_YPOS                = INTERNET_PRESENCES_LINE_YPOS + INTERNET_PRESENCES_LINE_SPACING;
-export const INTERNET_PRESENCES_SIZE                = 10;
+export const INTERNET_PRESENCES_SIZE                = STANDARD_FONT_SIZE;
