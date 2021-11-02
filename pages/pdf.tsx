c8a965c43b9b584a8655813dd5ddc21c24aa5994
a2f0 <dansullivan@gmail.com>
2021-11-02 17:37:31 -0400

    fix component name (#113)

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -1,11 +1,11 @@
-import Layout from '../components/layout/Main';
+import Main from '../components/layout/Main';
 import PdfResume from '../components/PdfResume';
 import React from 'react';
 
 export default function Pdf() {
   return (
-    <Layout>
+    <Main>
       <PdfResume />
-    </Layout>
+    </Main>
   );
 }
