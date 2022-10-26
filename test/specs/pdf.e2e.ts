0331ba1e561264e35c9987952cc23a38a89d9367
Dan Sullivan <dansullivan@gmail.com>
2022-10-26 08:37:25 -0400

    test: update test logic

--- test/specs/pdf.e2e.ts
+++ test/specs/pdf.e2e.ts
@@ -1,6 +1,5 @@
 import * as Constants from '../../constants';
 import PdfPage from '../pageobjects/pdf.page';
-import assert from 'assert';
 import path from 'path';
 import waitForFileExists from '../lib/fs';
 
@@ -11,27 +10,15 @@ describe('PDF Resume', () => {
   });
   it('should download a pdf', async () => {
     await PdfPage.open();
-    expect(PdfPage.fileMenuButton).toBeExisting();
-    expect(PdfPage.fileMenuItems).toBeExisting();
-    const fileMenuButton = await PdfPage.fileMenuButton;
-    const fileMenuItems = await PdfPage.fileMenuItems;
-    const downloadPdfMenuOption = await PdfPage.downloadPdfMenuOption;
-    let fileVisibility;
-    let pdfDownloadVisibility;
-    fileVisibility = await fileMenuItems.getCSSProperty('visibility');
-    assert.strictEqual(fileVisibility.value, 'hidden');
-    pdfDownloadVisibility = await downloadPdfMenuOption.getCSSProperty(
-      'visibility'
-    );
-    assert.strictEqual(pdfDownloadVisibility.value, 'hidden');
-    fileMenuButton.click();
-    fileVisibility = await fileMenuItems.getCSSProperty('visibility');
-    assert.strictEqual(fileVisibility.value, 'visible');
-    pdfDownloadVisibility = await downloadPdfMenuOption.getCSSProperty(
-      'visibility'
-    );
-    assert.strictEqual(pdfDownloadVisibility.value, 'visible');
-    downloadPdfMenuOption.click();
+    await expect(PdfPage.fileMenuButton).toBeExisting();
+    await expect(PdfPage.fileMenuItems).toBeExisting();
+    await expect(PdfPage.fileMenuItems).not.toBeDisplayed();
+    await expect(PdfPage.downloadPdfMenuOption).not.toBeDisplayed();
+    await expect(PdfPage.fileMenuButton).toBeClickable();
+    await PdfPage.fileMenuButton.click();
+    await expect(PdfPage.fileMenuItems).toBeDisplayed();
+    await expect(PdfPage.downloadPdfMenuOption).toBeDisplayed();
+    PdfPage.downloadPdfMenuOption.click();
     const filePath = path.join(
       Constants.TEST_DOWNLOAD_DIR,
       'dan.sullivan.resume.pdf'
