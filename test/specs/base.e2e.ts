28724afd88e7ab6d785608ba1142da8c9699cb09
Dan Sullivan <dansullivan@gmail.com>
2024-01-06 14:33:11 -0500

    refactor: use destructuring syntax

--- test/specs/base.e2e.ts
+++ test/specs/base.e2e.ts
@@ -2,34 +2,36 @@ import {BasePage} from '../pageobjects/base';
 
 describe('Menu Behavior', () => {
   it('should show/hide based on clicking and hovering', async () => {
-    await BasePage.open('');
-    await expect(BasePage.fileMenuButton).toBeExisting();
-    await expect(BasePage.fileMenuItems).toBeExisting();
+    const {fileMenuButton, fileMenuItems, viewMenuItems, viewMenuButton, open} =
+      BasePage;
+    await open('');
+    await expect(fileMenuButton).toBeExisting();
+    await expect(fileMenuItems).toBeExisting();
     // Click the File menu to show it, then click it again to hide it.
-    await expect(BasePage.fileMenuItems).not.toBeDisplayed();
-    BasePage.fileMenuButton.click();
-    await expect(BasePage.fileMenuItems).toBeDisplayed();
-    BasePage.fileMenuButton.click();
-    await expect(BasePage.fileMenuItems).not.toBeDisplayed();
+    await expect(fileMenuItems).not.toBeDisplayed();
+    fileMenuButton.click();
+    await expect(fileMenuItems).toBeDisplayed();
+    fileMenuButton.click();
+    await expect(fileMenuItems).not.toBeDisplayed();
 
     // Click the View menu to show it, then click it again to hide it.
-    await expect(BasePage.viewMenuItems).not.toBeDisplayed();
-    BasePage.viewMenuButton.click();
-    await expect(BasePage.viewMenuItems).toBeDisplayed();
-    BasePage.viewMenuButton.click();
-    await expect(BasePage.viewMenuItems).not.toBeDisplayed();
+    await expect(viewMenuItems).not.toBeDisplayed();
+    viewMenuButton.click();
+    await expect(viewMenuItems).toBeDisplayed();
+    viewMenuButton.click();
+    await expect(viewMenuItems).not.toBeDisplayed();
 
     // Click the File menu to show it, then hover the View menu to hide the File menu.
-    await expect(BasePage.fileMenuItems).not.toBeDisplayed();
-    BasePage.fileMenuButton.click();
-    await expect(BasePage.fileMenuItems).toBeDisplayed();
-    await BasePage.viewMenuButton.moveTo();
-    await BasePage.viewMenuItems.waitForDisplayed();
-    await expect(BasePage.fileMenuItems).not.toBeDisplayed();
+    await expect(fileMenuItems).not.toBeDisplayed();
+    fileMenuButton.click();
+    await expect(fileMenuItems).toBeDisplayed();
+    await viewMenuButton.moveTo();
+    await viewMenuItems.waitForDisplayed();
+    await expect(fileMenuItems).not.toBeDisplayed();
 
     //Hover back to the File menu to show it, and make sure the View menu hides.
-    await BasePage.fileMenuButton.moveTo();
-    await BasePage.fileMenuButton.waitForDisplayed();
-    await expect(BasePage.viewMenuItems).not.toBeDisplayed();
+    await fileMenuButton.moveTo();
+    await fileMenuButton.waitForDisplayed();
+    await expect(viewMenuItems).not.toBeDisplayed();
   });
 });
