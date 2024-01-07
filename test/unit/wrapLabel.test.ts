470876125477f7ebcf535ba92dd9b9b8e37d772a
a2f0 <dansullivan@gmail.com>
2024-01-07 09:26:01 -0500

    test: wrap long accomplishment (#707)
    * test: wrap long accomplishment
    
    * test: assert test strings

--- test/unit/wrapLabel.test.ts
+++ test/unit/wrapLabel.test.ts
@@ -18,5 +18,26 @@ describe('wrapLabel', () => {
     expect(window.innerWidth).toBe(1024);
     expect(window.innerHeight).toBe(768);
     expect(lines.length).toBe(1);
+    expect(lines[0]).toBe('This is a short accomplishment.');
+  });
+  test('it wraps a long accomplishment.', () => {
+    const accomplishmentFont = getFontString(
+      Constants.POSITION_ACCOMPLISHMENT_WEIGHT,
+      Constants.POSITION_ACCOMPLISHMENT_SIZE,
+      Constants.UNITS,
+      Constants.FONT_FAMILY
+    );
+    const {lines} = wrapLabel(
+      'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to make the test pass.',
+      Constants.POSITION_ACCOMPLISHMENT_MAX_WIDTH,
+      accomplishmentFont
+    );
+    expect(window.innerWidth).toBe(1024);
+    expect(window.innerHeight).toBe(768);
+    expect(lines.length).toBe(2);
+    expect(lines[0]).toBe(
+      'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to'
+    );
+    expect(lines[1]).toBe('make the test pass.');
   });
 });
