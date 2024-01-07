a058647626871a5184c81145d219ac055a053988
a2f0 <dansullivan@gmail.com>
2024-01-07 16:21:57 -0500

    feat: link extractor (#708)

--- test/unit/wrapLabel.test.ts
+++ test/unit/wrapLabel.test.ts
@@ -1,43 +1,57 @@
 import * as Constants from '../../constants';
 import {describe, expect, test} from 'vitest';
-import {getFontString, wrapLabel} from '../../lib/textUtils';
+import {extractLinks, getFontString, wrapLabel} from '../../lib/textUtils';
 
-describe('wrapLabel', () => {
-  test('it does not wrap a short accomplishment.', () => {
-    const accomplishmentFont = getFontString(
-      Constants.POSITION_ACCOMPLISHMENT_WEIGHT,
-      Constants.POSITION_ACCOMPLISHMENT_SIZE,
-      Constants.UNITS,
-      Constants.FONT_FAMILY
-    );
-    const {lines} = wrapLabel(
-      'This is a short accomplishment.',
-      Constants.POSITION_ACCOMPLISHMENT_MAX_WIDTH,
-      accomplishmentFont
-    );
-    expect(window.innerWidth).toBe(1024);
-    expect(window.innerHeight).toBe(768);
-    expect(lines.length).toBe(1);
-    expect(lines[0]).toBe('This is a short accomplishment.');
+describe('textUtils', () => {
+  describe('wrapLabel', () => {
+    test('it does not wrap a short accomplishment.', () => {
+      const accomplishmentFont = getFontString(
+        Constants.POSITION_ACCOMPLISHMENT_WEIGHT,
+        Constants.POSITION_ACCOMPLISHMENT_SIZE,
+        Constants.UNITS,
+        Constants.FONT_FAMILY
+      );
+      const {lines} = wrapLabel(
+        'This is a short accomplishment.',
+        Constants.POSITION_ACCOMPLISHMENT_MAX_WIDTH,
+        accomplishmentFont
+      );
+      expect(window.innerWidth).toBe(1024);
+      expect(window.innerHeight).toBe(768);
+      expect(lines.length).toBe(1);
+      expect(lines[0]).toBe('This is a short accomplishment.');
+    });
+    test('it wraps a long accomplishment.', () => {
+      const accomplishmentFont = getFontString(
+        Constants.POSITION_ACCOMPLISHMENT_WEIGHT,
+        Constants.POSITION_ACCOMPLISHMENT_SIZE,
+        Constants.UNITS,
+        Constants.FONT_FAMILY
+      );
+      const {lines} = wrapLabel(
+        'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to make the test pass.',
+        Constants.POSITION_ACCOMPLISHMENT_MAX_WIDTH,
+        accomplishmentFont
+      );
+      expect(window.innerWidth).toBe(1024);
+      expect(window.innerHeight).toBe(768);
+      expect(lines.length).toBe(2);
+      expect(lines[0]).toBe(
+        'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to'
+      );
+      expect(lines[1]).toBe('make the test pass.');
+    });
   });
-  test('it wraps a long accomplishment.', () => {
-    const accomplishmentFont = getFontString(
-      Constants.POSITION_ACCOMPLISHMENT_WEIGHT,
-      Constants.POSITION_ACCOMPLISHMENT_SIZE,
-      Constants.UNITS,
-      Constants.FONT_FAMILY
-    );
-    const {lines} = wrapLabel(
-      'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to make the test pass.',
-      Constants.POSITION_ACCOMPLISHMENT_MAX_WIDTH,
-      accomplishmentFont
-    );
-    expect(window.innerWidth).toBe(1024);
-    expect(window.innerHeight).toBe(768);
-    expect(lines.length).toBe(2);
-    expect(lines[0]).toBe(
-      'This is a long accomplishment. It definitely wraps more than one line, intentionally of course, to'
-    );
-    expect(lines[1]).toBe('make the test pass.');
+  describe('extractLinks', () => {
+    test('it extracts a link with an index position', () => {
+      const result = extractLinks('This is [google](https://google.com).');
+      expect(result).not.toBe([]);
+      expect(result.length).toBe(1);
+      const {text, url, index, plainString} = result[0];
+      expect(text).toBe('google');
+      expect(url).toBe('https://google.com');
+      expect(index).toBe(8);
+      expect(plainString).toBe('This is google.');
+    });
   });
 });
