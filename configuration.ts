cc36f6eb0b94418e22eb6d70c6caed78011c37e5
a2f0 <dansullivan@gmail.com>
2024-01-09 12:41:36 -0500

    Migrate config (#718)
    * refactor: name in config
    
    * chore: move address line

--- configuration.ts
+++ configuration.ts
@@ -6,24 +6,36 @@ interface ICoordinate {
 interface IResumeConfiguration {
   documentWidth: number;
   documentHeight: number;
+  standardFontSize: number;
   startX: number;
   startY: number;
+  units: string;
+  fontFamily: string;
   namePos: ICoordinate;
   nameSize: number;
+  nameWeight: number;
   nameYPosMiddle: number;
   leftPanelPos: ICoordinate;
-  rightPanelPos: ICoordinate;
   leftPanelPercentage: number;
-  fontFamily: string;
+  leftPanelWidth: number;
+  rightPanelPos: ICoordinate;
+  rightPanelPercentage: number;
+  rightPanelWidth: number;
+  addressPos: ICoordinate;
+  addressYPosMiddle: number;
+  addressSize: number;
 }
 
 class ResumeConfiguration implements IResumeConfiguration {
   documentWidth = 612;
   documentHeight = 792;
+  standardFontSize = 9.75;
   startX = 18; // .25 inches from edge of page
   startY = 18; // .25 inches from edge of page
+  units = 'pt';
   fontFamily = 'Helvetica';
   namePos = {x: this.startX, y: this.startY};
+  nameWeight = 400;
   nameSize = 15;
   nameYPosMiddle = this.namePos.y + this.nameSize / 2;
   leftPanelPos = {x: 0, y: 0};
@@ -32,6 +44,10 @@ class ResumeConfiguration implements IResumeConfiguration {
   rightPanelPos = {x: this.leftPanelWidth, y: 0};
   rightPanelPercentage = 1 - this.leftPanelPercentage;
   rightPanelWidth = this.rightPanelPercentage * this.documentWidth;
+  addressPos = {x: this.startX, y: this.startY + this.nameSize};
+  addressSize = this.standardFontSize;
+  addressYPosMiddle = this.addressPos.y + this.addressSize / 2;
+  addressWeight = 400;
 }
 
 const resumeConfiguration = new ResumeConfiguration();
