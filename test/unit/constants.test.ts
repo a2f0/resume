01dacd9ffeda5c06b2c92e9c32ff9f30aaecdb99
Dan Sullivan <dansullivan@gmail.com>
2024-01-11 10:48:23 -0500

    update: use configuration in unit test

--- test/unit/constants.test.ts
+++ test/unit/constants.test.ts
@@ -1,5 +1,7 @@
-import * as Constants from '../../constants';
 import {describe, expect, it} from 'vitest';
+import {resumeConfiguration} from '../../configuration';
+
+const {standardFontSize} = resumeConfiguration;
 
 describe('Constants Behavior', () => {
   // Use a point size that converts directly to pixels because there are browser quirks where a fractional pixel
@@ -9,7 +11,7 @@ describe('Constants Behavior', () => {
   // https://stackoverflow.com/questions/46653569/canvas-measuretext-differences-on-browsers-are-huge
   it('should use a point size that does not convert to fractional pixels', async () => {
     // each pt is 1.3333 px
-    const fontSizeInPixels = Constants.STANDARD_FONT_SIZE * (1 + 1 / 3);
+    const fontSizeInPixels = standardFontSize * (1 + 1 / 3);
     const remainder = fontSizeInPixels % 1;
     expect(remainder).toBe(0);
   });
