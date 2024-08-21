e4aa79b4de3f2ec3dad47c8adeb40443377e6b8d
Dan Sullivan <dansullivan@gmail.com>
2024-08-20 21:11:04 -0400

    chore: install canvas

--- __tests__/vitest.setup.ts
+++ __tests__/vitest.setup.ts
@@ -1,4 +1,9 @@
 import {afterEach} from 'vitest';
 import {cleanup} from '@testing-library/react';
 
+import {createCanvas} from 'canvas';
+
+// Add the canvas implementation to the global scope
+global.HTMLCanvasElement = createCanvas().constructor;
+
 afterEach(cleanup);
